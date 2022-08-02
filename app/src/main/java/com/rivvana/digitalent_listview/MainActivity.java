15ec958a1e44cafb924b43f583951ee560c5a9fc
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-02 18:44:10 +0700

    add function

--- app/src/main/java/com/rivvana/digitalent_listview/MainActivity.java
+++ app/src/main/java/com/rivvana/digitalent_listview/MainActivity.java
@@ -3,12 +3,38 @@ package com.rivvana.digitalent_listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvItem;
+    private String[] namaNegara = new String[]{
+            "Indonesia", "Malaysia", "Singapura", "Laos",
+            "Brunei Darussalam", "Inggris", "Italia"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("List View Sederhana");
+
+        lvItem = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namaNegara);
+
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : "+namaNegara[position], Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
